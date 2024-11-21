88e0d010aa57d6165697ab9299ff4422ad1a4f3a
name <email@gmail.com>
2024-11-21 10:23:45 +0100

    zad2

---
+++ zad3/zad3.js
@@ -0,0 +1,20 @@
+function range(a,b){
+    const wynik = [];
+    if(a>b){
+        for(let i=a; i>=b; i--)
+        {
+            wynik.push(i);
+        }
+    }
+    else{
+        for(let i=a; i<=b; i++)
+        {
+            wynik.push(i);
+        }
+    }
+    return wynik;
+    
+}
+
+console.log(range(1,5));
+console.log(range(5,1));
