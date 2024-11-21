2f709bec20205a099fc5f34d6112cc0e3796d497
name <email@gmail.com>
2024-11-21 08:52:25 +0100

    zad1

---
+++ zad1/functions.js
@@ -0,0 +1,12 @@
+function isOdd(x){
+    return x%2!=0;
+}
+
+const isEven = (x) =>{
+    return x%2==0;
+}
+
+console.log(isOdd(9));
+console.log(isOdd(10));
+console.log(isEven(9));
+console.log(isEven(10));
